c068ee8e9452cb45b6466a03afaf7df477cfa99c
JunSeo Kwak <Ruggellia@google.com>
2019-04-17 00:23:22 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customer from './components/Customer'
+import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from'@material-ui/core/TableHead';
+import TableBody from'@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme=>({
+
+  root :{
+     width: '100%',
+     marginTop: theme.spacing.unit*3,
+     overflowX : "auto"
+
+  },
+
+  table:{
+    minWidth : 1088
+
+  }
+
+
+}
+
+
+
+)
 
 const customers = [
   {
@@ -39,19 +67,29 @@ const customers = [
 class App extends Component {
 
   render() {
+    const {classes} = this.props;
      return ( 
-       <div>
-       {
-       customers.map(c =>{
-         return(
-           <Customer id={c.id} image={c.image} name={c.name} gender={c.gender} birthday={c.birthday} job={c.job} />
-         );
+       <Paper className ={classes.root}>
+       <Table className ={classes.table} >
+        <TableHead>
+          <TableRow>
+            <TableCell>아이디</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
 
-       })
-      }
-       </div>
+         <TableBody>
+           {customers.map(c =>{ return( <Customer id={c.id} image={c.image} name={c.name} gender={c.gender} birthday={c.birthday} job={c.job} />);})}
+         </TableBody>
+       </Table>
+       
+       </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
